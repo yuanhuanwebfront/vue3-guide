0cf8882a4c5a8a290daaae7da2eb330c3b5ef6c7
yuanhuan <894881283@qq.com>
2023-12-28 19:58:50 +0800

    *

--- src/composition/menu/config.js
+++ src/composition/menu/config.js
@@ -1,3 +1,5 @@
+import YoutubeIcon from '@/assets/icon/youtube.png';
+
 const menuOptions = [
   {
     label: '在线视频',
@@ -5,8 +7,22 @@ const menuOptions = [
     icon: 'icon-video',
     list: [
       {
-        label: 'youtube',
-        icon: ''
+        label: '国外视频',
+        list: [
+          {
+            label: 'youtube',
+            icon: YoutubeIcon
+          }
+        ],
+      },
+      {
+        label: '国内视频',
+        list: [
+          {
+            label: '腾讯视频',
+            icon: ''
+          }
+        ]
       }
     ]
   },
--- src/composition/menu/index.js
+++ src/composition/menu/index.js
@@ -1,10 +1,16 @@
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
+import { useRoute } from 'vue-router';
 import menuOptions from './config';
 
 function useMenu(){
   const menus = ref(menuOptions);
+  const path = useRoute().path;
 
-  return { menus };
+  const cptPathMenus = computed(() => {
+    return menus.value.find(item => item.path == path)
+  })
+
+  return { menus, cptPathMenus };
 }
 
 export default useMenu;
